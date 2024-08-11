558be3f0d72f496b255f428931206a807c0a0fdb
hesham-afifi <hesham.mohamed@widebot.net>
2024-08-12 01:07:17 +0300

    trying to fix deplyments errors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   app.setGlobalPrefix('api'); // Ensure the prefix is set correctly
 
