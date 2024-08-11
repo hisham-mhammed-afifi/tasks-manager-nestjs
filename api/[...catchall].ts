45c0e8a42225dd4be2f2244f4a549b11a3ed331d
hesham-afifi <hesham.mohamed@widebot.net>
2024-08-12 02:02:54 +0300

    ||||

--- api/[...catchall].ts
+++
@@ -1,6 +0,0 @@
-import bootstrap from '../src/main';
-
-export default async function handler(req, res) {
-  const app = await bootstrap();
-  app(req, res); // Pass the request and response to the Express server
-}
